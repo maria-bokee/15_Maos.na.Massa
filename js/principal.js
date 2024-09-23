c24b2a1adec1e81706598cb08a0e0074e044c40d
maria-bokee <140661706+maria-bokee@users.noreply.github.com>
2024-09-23 08:41:27 -0300

    falta terminar

---
+++ js/principal.js
@@ -0,0 +1,5 @@
+var titulo = document.querySelector ("h1");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll('.paciente');
+for (let contador = 0; contador < pacientes.length; contador++)
