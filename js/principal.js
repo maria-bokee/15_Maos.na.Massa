1f11e282a3cbef90c3377586ec6ea364db69e3ac
maria-bokee <140661706+maria-bokee@users.noreply.github.com>
2024-09-24 08:08:32 -0300

    terminai

--- js/principal.js
+++ js/principal.js
@@ -2,4 +2,9 @@ var titulo = document.querySelector ("h1");
 titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll('.paciente');
-for (let contador = 0; contador < pacientes.length; contador++)
+for (let contador = 0; contador < pacientes.length; contador++) {
+    var peso = pacientes [contador].querySelector ('.info-peso').textContent
+    var altura = pacientes [contador].querySelector ('.info-altura').textContent
+    var imc = peso / (altura * altrura);
+    pacientes [contador].querySelector('.info=imc').textContent = imc;
+}
