08b4e809ed39601934ca5416af8ee13443b0f0f3
maria-bokee <140661706+maria-bokee@users.noreply.github.com>
2024-09-25 08:06:22 -0300

    TERMINADO

--- js/principal.js
+++ js/principal.js
@@ -5,6 +5,6 @@ var pacientes = document.querySelectorAll('.paciente');
 for (let contador = 0; contador < pacientes.length; contador++) {
     var peso = pacientes [contador].querySelector ('.info-peso').textContent
     var altura = pacientes [contador].querySelector ('.info-altura').textContent
-    var imc = peso / (altura * altrura);
-    pacientes [contador].querySelector('.info=imc').textContent = imc;
+    var imc = peso / (altura * altura);
+    pacientes [contador].querySelector('.info-imc').textContent = imc;
 }
